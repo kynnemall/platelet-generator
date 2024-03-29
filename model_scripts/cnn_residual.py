698d5370ba68555c24e9861b2d34ceb74223476e
martin <sideproject1892@gmail.com>
2024-03-29 10:09:20 +0000

    Encoder and Decoder architectures v1

---
+++ model_scripts/cnn_residual.py
@@ -0,0 +1,93 @@
+from torch import nn
+from pythae.models.nn import BaseEncoder, BaseDecoder
+from pythae.models.base.base_utils import ModelOutput
+
+class DepthwiseSeparableConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding='same'):
+        super(DepthwiseSeparableConv, self).__init__()
+        self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size, stride, padding, groups=in_channels)
+        self.pointwise = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.depthwise(x)
+        x = self.pointwise(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        return x
+
+
+class ResConvEncoder(BaseEncoder):
+    def __init__(self, args):
+        BaseEncoder.__init__(self)
+        self.name = 'ResConv_v1'
+        self.l1 = DepthwiseSeparableConv(in_channels=1, out_channels=32, kernel_size=3, stride=1)
+        self.rl1 = DepthwiseSeparableConv(in_channels=1, out_channels=32, kernel_size=1, stride=1)
+        self.l2 = DepthwiseSeparableConv(in_channels=32, out_channels=64, kernel_size=3, stride=1)
+        self.rl2 = DepthwiseSeparableConv(in_channels=32, out_channels=64, kernel_size=1, stride=1)
+        self.l3 = DepthwiseSeparableConv(in_channels=64, out_channels=128, kernel_size=3, stride=1)
+        self.rl3 = DepthwiseSeparableConv(in_channels=64, out_channels=128, kernel_size=1, stride=1)
+        self.l4 = DepthwiseSeparableConv(in_channels=128, out_channels=256, kernel_size=3, stride=1)
+        self.rl4 = DepthwiseSeparableConv(in_channels=128, out_channels=256, kernel_size=1, stride=1)
+        self.max_pool = nn.MaxPool2d(2)
+        self.flat = nn.Flatten()
+        self.embedding = nn.Linear(256 * args.latent_dim, args.latent_dim)
+        self.log_var = nn.Linear(256 * args.latent_dim, args.latent_dim)
+
+    def forward(self, x):
+
+        x = self.l1(x) + self.rl1(x)
+        x = self.max_pool(x)
+        x = self.l2(x) + self.rl2(x)
+        x = self.max_pool(x)
+        x = self.l3(x) + self.rl3(x)
+        x = self.max_pool(x)
+        x = self.l4(x) + self.rl4(x)
+        x = self.max_pool(x)
+
+        x = self.flat(x)
+        output = ModelOutput(
+            embedding=self.embedding(x),
+            log_covariance=self.log_var(x)
+        )
+        return output
+
+
+class ResConvDecoder(BaseDecoder):
+    def __init__(self, args):
+        BaseDecoder.__init__(self)
+        self.name = 'ResConv_v1'
+        self.latent_features = args.latent_dim
+
+        self.l1 = DepthwiseSeparableConv(in_channels=args.latent_dim, out_channels=256, kernel_size=3, stride=1)
+        self.rl1 = DepthwiseSeparableConv(in_channels=args.latent_dim, out_channels=256, kernel_size=1, stride=1)
+        self.l2 = DepthwiseSeparableConv(in_channels=256, out_channels=128, kernel_size=3, stride=1)
+        self.rl2 = DepthwiseSeparableConv(in_channels=256, out_channels=128, kernel_size=1, stride=1)
+        self.l3 = DepthwiseSeparableConv(in_channels=128, out_channels=64, kernel_size=3, stride=1)
+        self.rl3 = DepthwiseSeparableConv(in_channels=128, out_channels=64, kernel_size=1, stride=1)
+        self.l4 = DepthwiseSeparableConv(in_channels=64, out_channels=32, kernel_size=3, stride=1)
+        self.rl4 = DepthwiseSeparableConv(in_channels=64, out_channels=32, kernel_size=1, stride=1)
+
+        self.final = nn.Conv2d(32, 1, kernel_size=1, padding='same')
+        self.final_act = nn.Sigmoid()
+
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.linear = nn.Linear(args.latent_dim, args.latent_dim * 10 * 10)
+
+    def forward(self, x):
+        x = self.linear(x)
+        x = x.view(x.size(0), self.latent_features, 10, 10)
+        x = self.l1(x) + self.rl1(x)
+        x = self.upsample(x)
+        x = self.l2(x) + self.rl2(x)
+        x = self.upsample(x)
+        x = self.l3(x) + self.rl3(x)
+        x = self.upsample(x)
+        x = self.l4(x) + self.rl4(x)
+        x = self.upsample(x)
+
+        x = self.final(x)
+        x = self.final_act(x)
+        output = ModelOutput(reconstruction=x)
+        return output
---
+++ model_scripts/vit.py
@@ -0,0 +1,57 @@
+import torch
+import torchvision
+from torch import nn
+from pythae.models.nn import BaseEncoder, BaseDecoder
+from pythae.models.base.base_utils import ModelOutput
+
+
+class TransformerEncoder(BaseEncoder):
+    def __init__(self, args=None):
+        img_width = 160
+        img_channels = 1
+        patch_size = 32
+        d_model = 64
+        num_heads = 4
+        num_layers = 3
+        num_latents = 100
+        ff_dim = 1024
+        # taken from tutorial at https://comsci.blog/posts/vit
+        BaseEncoder.__init__(self)
+        self.name = 'ViT_v1'
+
+        self.patch_size = patch_size
+        self.patch_embedding = nn.Linear(img_channels * patch_size * patch_size, d_model)
+        self.cls_token = nn.Parameter(torch.randn(1, 1, d_model))
+
+        # + 1 because we add cls tokens
+        self.position_embedding = nn.Parameter(
+            torch.rand(1, (img_width // patch_size) * (img_width // patch_size) + 1, d_model)
+        )
+
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model, nhead=num_heads, dim_feedforward=ff_dim, batch_first=True
+        )
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
+        self.fc = nn.Linear(d_model, num_latents)
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+
+        # we divide the image into patches then flatten and embed them
+        x = x.unfold(2, self.patch_size, self.patch_size).unfold(3, self.patch_size, self.patch_size)
+        x = x.contiguous().view(N, C, -1, self.patch_size, self.patch_size)
+        x = x.permute(0, 2, 3, 4, 1).contiguous().view(N, -1, C * self.patch_size * self.patch_size)
+        x = self.patch_embedding(x)
+
+        # cls tokens concatenated after repeating it for the batch
+        cls_tokens = self.cls_token.repeat(N, 1, 1)
+        x = torch.cat((cls_tokens, x), dim=1)
+        x = x + self.position_embedding
+        x = self.transformer_encoder(x)
+
+        # only taking the transformed output of the cls token
+        x = x[:, 0]
+
+        # compress to latent space
+        x = self.fc(x)
+        return x
